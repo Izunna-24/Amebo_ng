e37988bf1a9002360db6e925cff9d4ebf5eb4897
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-26 06:59:54 +0100

    java

--- src/main/java/nation/amebo/data/model/Comment.java
+++ src/main/java/nation/amebo/data/model/Comment.java
@@ -1,10 +1,20 @@
 package nation.amebo.data.model;
 
-import java.time.LocalDateTime;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+@Data
+@Document("Comments")
 public class Comment {
+    @Id
     private String id;
+
+    @DBRef
     private User commenter;
+
     private String commentBody;
     private LocalDateTime timeOfComment;
 
