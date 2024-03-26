1fa8619a3cd42c9b0701549f243673a5b2b68e64
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-26 07:07:08 +0100

    java

--- src/main/java/nation/amebo/data/model/User.java
+++ src/main/java/nation/amebo/data/model/User.java
@@ -1,13 +1,23 @@
 package nation.amebo.data.model;
 
-import java.util.List;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+@Data
+@Document("Users")
 public class User {
     private String firstname;
     private String lastname;
     private String password;
     private String username;
+
+    @Id
     private String id;
+
+    @DBRef
     private List<Post> posts;
 
 }
