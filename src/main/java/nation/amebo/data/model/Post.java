62d51846bcbb82b29c398171e9655ce31ab35fb6
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-26 01:11:27 +0100

    java

--- src/main/java/nation/amebo/data/model/Post.java
+++ src/main/java/nation/amebo/data/model/Post.java
@@ -1,4 +1,17 @@
 package nation.amebo.data.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Post {
+private String title;
+private String content;
+private String id;
+private LocalDateTime dateCreated;
+private List<View> views;
+private List<Comment> comments;
 }
+
+
+
+
