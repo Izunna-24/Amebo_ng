2e64f08f9f04eb508f130d6fc91516e3661da658
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-26 07:07:21 +0100

    java

--- src/main/java/nation/amebo/data/model/View.java
+++ src/main/java/nation/amebo/data/model/View.java
@@ -1,9 +1,20 @@
 package nation.amebo.data.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
+@Data
+@Document("Views")
 
 public class View {
     private LocalDateTime timeOfView;
+
+    @DBRef
     private User viewer;
+
+    @Id
     private String id;
 }
