ce83e21817449d482c8eace8230a98ad5ef95078
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-26 01:20:29 +0100

    java

--- src/main/java/nation/amebo/data/model/User.java
+++ src/main/java/nation/amebo/data/model/User.java
@@ -1,4 +1,13 @@
 package nation.amebo.data.model;
 
+import java.util.List;
+
 public class User {
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String username;
+    private String id;
+    private List<Post> posts;
+
 }
