279f0dc82316a32a65e6bd6bb9ec8c9e23d61242
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-26 01:20:58 +0100

    java

--- src/main/java/nation/amebo/data/model/View.java
+++ src/main/java/nation/amebo/data/model/View.java
@@ -1,4 +1,9 @@
 package nation.amebo.data.model;
 
+import java.time.LocalDateTime;
+
 public class View {
+    private LocalDateTime timeOfView;
+    private User viewer;
+    private String id;
 }
