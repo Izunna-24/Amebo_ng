baea9e1d4a5c8ca9e784bed31f17bdfb717ab47e
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-26 01:20:14 +0100

    java

--- src/main/java/nation/amebo/data/model/Comment.java
+++ src/main/java/nation/amebo/data/model/Comment.java
@@ -1,4 +1,11 @@
 package nation.amebo.data.model;
 
+import java.time.LocalDateTime;
+
 public class Comment {
+    private String id;
+    private User commenter;
+    private String commentBody;
+    private LocalDateTime timeOfComment;
+
 }
