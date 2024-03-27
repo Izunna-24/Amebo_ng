292b4d85ab86fe2c1d9f17a4df1cc85879450e60
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-27 02:02:04 +0100

    java

---
+++ src/main/java/nation/amebo/services/PostServiceImp.java
@@ -0,0 +1,10 @@
+package nation.amebo.services;
+
+import nation.amebo.dto.CreatePostRequest;
+
+public class PostServiceImp implements PostService{
+    @Override
+    public void createPost(CreatePostRequest createPostRequest) {
+
+    }
+}
