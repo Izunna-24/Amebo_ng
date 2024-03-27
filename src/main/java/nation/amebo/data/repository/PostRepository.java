8536494c215b8830b6ab928ef60379fadd6c1ba9
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-27 01:51:28 +0100

    java

---
+++ src/main/java/nation/amebo/data/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package nation.amebo.data.repository;
+
+import nation.amebo.data.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
+
+
