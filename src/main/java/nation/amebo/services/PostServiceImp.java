990129443a752d9ca913633fb5a144aeb070f0b0
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-27 02:04:09 +0100

    java

--- src/main/java/nation/amebo/services/PostServiceImp.java
+++ src/main/java/nation/amebo/services/PostServiceImp.java
@@ -1,8 +1,15 @@
 package nation.amebo.services;
 
+import nation.amebo.data.repository.PostRepository;
 import nation.amebo.dto.CreatePostRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PostServiceImp implements PostService{
+
+    @Autowired
+    private PostRepository postRepository;
     @Override
     public void createPost(CreatePostRequest createPostRequest) {
 
