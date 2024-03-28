4e05a89d8775729a75469fa13dffc739e8cbf502
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-28 03:36:24 +0100

    java

--- src/main/java/nation/amebo/services/PostServiceImp.java
+++ src/main/java/nation/amebo/services/PostServiceImp.java
@@ -1,7 +1,6 @@
 package nation.amebo.services;
 
-import nation.amebo.data.model.Post;
-import nation.amebo.data.repository.PostRepository;
+import nation.amebo.data.repositories.PostRepository;
 import nation.amebo.dto.CreatePostRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
