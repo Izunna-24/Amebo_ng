981e4c9ce7afa7e1f62ec6a52051343c98f0ec2e
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-28 05:59:01 +0100

    java

--- src/test/java/nation/amebo/services/UserServicesImpTest.java
+++ src/test/java/nation/amebo/services/UserServicesImpTest.java
@@ -1,13 +1,18 @@
 package nation.amebo.services;
 
+
 import nation.amebo.data.repositories.UserRepository;
 import nation.amebo.dto.RegisterRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class UserServicesImpTest {
+    @Autowired
+    private UserServices userServices;
+
     @Test
     public void registerUser_usersCountIsOneTest() {
         RegisterRequest registerRequest = new RegisterRequest();
@@ -15,7 +20,8 @@ class UserServicesImpTest {
         registerRequest.setLastname("Hamcrest");
         registerRequest.setUsername("username");
         registerRequest.setPassword("password");
-        UserServices.registerUserWith(registerRequest);
+        userServices.registerUserWith(registerRequest);
+        assertEquals(1, userServices.numberOfRegisteredUsers());
 
     }
 }
