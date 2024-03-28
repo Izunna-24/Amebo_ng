80bc5cdf892ee625a34f95de8ff040d68ff06db0
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-28 05:58:09 +0100

    java

--- src/main/java/nation/amebo/dto/RegisterRequest.java
+++ src/main/java/nation/amebo/dto/RegisterRequest.java
@@ -1,8 +1,10 @@
 package nation.amebo.dto;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document
 public class RegisterRequest {
     private String firstname;
     private String lastname;
