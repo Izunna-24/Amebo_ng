993885b9806d4df18bb85167dfc1fed6fca33f59
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-01 09:34:52 +0100

    java

--- src/main/java/nation/amebo/controllers/UserController.java
+++ src/main/java/nation/amebo/controllers/UserController.java
@@ -1,4 +1,28 @@
 package nation.amebo.controllers;
 
+import nation.amebo.dtos.requests.RegisterUserRequest;
+import nation.amebo.excetptions.AmeboNationBlogExceptions;
+import nation.amebo.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+    @Autowired
+    private UserServices userServices;
+
+@PostMapping("/register")
+    public String registerUser(@RequestBody RegisterUserRequest registerUserRequest){
+
+        try {
+            userServices.registerUserWith(registerUserRequest);
+            return String.format("%s your registration to Amebo_ng is successful", registerUserRequest.getUsername());
+        } catch (AmeboNationBlogExceptions error){
+            return error.getMessage();
+        }
+    }
 }
