97843c7eaf2e6ba86c3c48842b4aa3b595607e0f
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-01 09:33:44 +0100

    java

---
+++ src/test/java/nation/amebo/services/PostServicesImplTest.java
@@ -0,0 +1,12 @@
+package nation.amebo.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServicesImplTest {
+
+    @Test
+    void createPost() {
+    }
+}
