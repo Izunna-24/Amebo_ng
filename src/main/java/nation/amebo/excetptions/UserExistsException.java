1e545e8466454ab0d9d27a5816ed0744cfe5d42e
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-01 09:35:15 +0100

    java

--- src/main/java/nation/amebo/excetptions/UserExistsException.java
+++ src/main/java/nation/amebo/excetptions/UserExistsException.java
@@ -1,6 +1,6 @@
 package nation.amebo.excetptions;
 
-public class UserExistsException extends AmeboNationExceptions{
+public class UserExistsException extends AmeboNationBlogExceptions {
     public UserExistsException(String message){
         super(message);
     }
