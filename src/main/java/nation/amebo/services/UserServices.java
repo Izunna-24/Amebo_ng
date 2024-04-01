8ada4bfed70a8af6c1f232b716c912c41b9e5e70
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-02 00:45:54 +0100

    java

--- src/main/java/nation/amebo/services/UserServices.java
+++ src/main/java/nation/amebo/services/UserServices.java
@@ -1,11 +1,13 @@
 package nation.amebo.services;
 
-import nation.amebo.dtos.requests.LoginUserRequest;
+import nation.amebo.data.models.User;
+import nation.amebo.dtos.requests.LoginRequest;
 import nation.amebo.dtos.requests.RegisterUserRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserServices {
 void registerUserWith(RegisterUserRequest registerUserRequest);
-void login(LoginUserRequest loginUserRequest);
+void login(LoginRequest loginRequest);
+User findByUsername(String username);
 }
