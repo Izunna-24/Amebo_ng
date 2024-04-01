dce21d30f3afdd2ef2704128ced9a3817443ef6d
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-01 09:31:06 +0100

    java

--- src/main/java/nation/amebo/dtos/requests/CreatePostRequest.java
+++ src/main/java/nation/amebo/dtos/requests/CreatePostRequest.java
@@ -3,11 +3,13 @@ package nation.amebo.dtos.requests;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Data
 @Document
 public class CreatePostRequest {
     private String title;
     private String content;
     private String poster;
-    //private LocalDateTime dateCreated;
+    private LocalDateTime dateCreated = LocalDateTime.now();
 }
