f040539add1cc892e5c9422b07eedd187bfc4ac6
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-02 00:46:16 +0100

    java

--- src/main/java/nation/amebo/services/UserServicesImpl.java
+++ src/main/java/nation/amebo/services/UserServicesImpl.java
@@ -2,11 +2,16 @@ package nation.amebo.services;
 
 import nation.amebo.data.models.User;
 import nation.amebo.data.repositories.UserRepository;
-import nation.amebo.dtos.requests.LoginUserRequest;
+import nation.amebo.dtos.requests.LoginRequest;
 import nation.amebo.dtos.requests.RegisterUserRequest;
+import nation.amebo.excetptions.UserDoesNotExistException;
 import nation.amebo.excetptions.UserExistsException;
+import nation.amebo.excetptions.WrongPasswordException;
+import nation.amebo.excetptions.WrongUserNameException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 import static nation.amebo.utilities.Mapper.map;
 
@@ -27,17 +32,40 @@ public class UserServicesImpl implements UserServices {
 
     private void validateRegistration(String username) {
         boolean userExists = userRepository.existsById(username);
-        if (userExists) throw new UserExistsException("User exists");
+        if (userExists) throw new UserExistsException(String.format("%s exists",username));
     }
 
 
     @Override
-    public void login(LoginUserRequest loginUserRequest) {
-    User user = new User();
-    user.setPassword(loginUserRequest.getPassword());
-    user.setUsername(loginUserRequest.getUsername());
+    public void login(LoginRequest loginRequest) {
+        User user = new User();
+        user.setPassword(loginRequest.getPassword());
+        user.setUsername(loginRequest.getUsername());
+        userRepository.findByUsername(loginRequest.getUsername());
+        loginValidation(loginRequest, user);
+        user.setLoggedIn(false);
+        userRepository.save(user);
     }
+
+    private static void loginValidation(LoginRequest loginRequest, User user) {
+        if (!user.getUsername().equalsIgnoreCase(loginRequest.getUsername())) {
+            throw new WrongUserNameException("Invalid login detail, try again!");
+        }
+        if (!user.getPassword().equals(loginRequest.getPassword())) {
+            throw new WrongPasswordException("Invalid login detail, try again");
+        }
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isEmpty()) throw new UserDoesNotExistException(String.format("%s is not found! ", username));
+     return user.get();
+    }
+
 }
+
+
 
 
 
