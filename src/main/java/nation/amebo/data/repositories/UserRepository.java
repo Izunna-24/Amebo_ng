b83ba9c8f314591fb037bd8c1a14bde716151f28
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-02 00:45:20 +0100

    java

--- src/main/java/nation/amebo/data/repositories/UserRepository.java
+++ src/main/java/nation/amebo/data/repositories/UserRepository.java
@@ -4,7 +4,9 @@ package nation.amebo.data.repositories;
 import nation.amebo.data.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends MongoRepository<User, String> {
-
+    Optional<User> findByUsername(String username);
 }
