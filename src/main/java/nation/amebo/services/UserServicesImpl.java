e2e3913c0fec3374d08239299bedb8e5377650ee
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-01 09:36:04 +0100

    java

--- src/main/java/nation/amebo/services/UserServicesImpl.java
+++ src/main/java/nation/amebo/services/UserServicesImpl.java
@@ -2,7 +2,8 @@ package nation.amebo.services;
 
 import nation.amebo.data.models.User;
 import nation.amebo.data.repositories.UserRepository;
-import nation.amebo.dto.RegisterRequest;
+import nation.amebo.dtos.requests.LoginUserRequest;
+import nation.amebo.dtos.requests.RegisterUserRequest;
 import nation.amebo.excetptions.UserExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,16 +16,26 @@ public class UserServicesImpl implements UserServices {
     private UserRepository userRepository;
 
     @Override
-    public void registerUserWith(RegisterRequest registerRequest) {
-        registerRequest.setUsername(registerRequest.getUsername().toLowerCase());
-        validateReg(registerRequest.getUsername());
-        User user = map(registerRequest);
+    public void registerUserWith(RegisterUserRequest registerUserRequest) {
+        registerUserRequest.setUsername(registerUserRequest.getUsername().toLowerCase());
+        validateRegistration(registerUserRequest.getUsername());
+        User user = map(registerUserRequest);
         userRepository.save(user);
     }
 
-    private void validateReg(String username) {
+
+
+    private void validateRegistration(String username) {
         boolean userExists = userRepository.existsById(username);
         if (userExists) throw new UserExistsException("User exists");
+    }
+
+
+    @Override
+    public void login(LoginUserRequest loginUserRequest) {
+    User user = new User();
+    user.setPassword(loginUserRequest.getPassword());
+    user.setUsername(loginUserRequest.getUsername());
     }
 }
 
